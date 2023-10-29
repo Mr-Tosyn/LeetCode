719ea40eb56eb18127964e54baa5da6f6770f246
Adesua Oluwatosin <51682725+Mr-Tosyn@users.noreply.github.com>
2023-10-29 14:23:54 +0100

    Time: 38 ms (67.77%), Space: 13.2 MB (32.17%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+class Solution(object):
+    def isPalindrome(self, x):
+        return str(x)==str(x)[::-1]       
