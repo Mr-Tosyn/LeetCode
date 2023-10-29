fae4bb4770fb46eb428f03f57adc738c882d10a9
Adesua Oluwatosin <51682725+Mr-Tosyn@users.noreply.github.com>
2023-10-29 14:28:39 +0100

    Time: 39 ms (16.6%), Space: 13.2 MB (57.38%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        counter = 0
+        special = {
+            'IV':4,
+            'IX':9,
+            'XL':40,
+            'XC':90,
+            'CD':400,
+            'CM':900,
+        }
+        normal = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        
+        for char, num in special.items():
+            if char in s:
+                counter+=num
+                s = s.replace(char, '')
+                if s=='':
+                    return counter
+                
+        for char, num in normal.items():
+            if char in s:
+                counter+=num*s.count(char)
+                s = s.replace(char, '')
+                if s=='':
+                    return counter
+                
+                
+        return counter        
